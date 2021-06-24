c436820578aee38a76bb720f257f4995359a34fd
SahilB689 <76981086+SahilB689@users.noreply.github.com>
2021-06-23 20:05:32 -0400

    Update __init__.py

--- pizza/__init__.py
+++ pizza/__init__.py
@@ -3,8 +3,8 @@ from .base import *
 env_name = os.getenv('ENV_NAME', 'local')
 
 if env_name == 'prod':
-from .prod import
-elif env_name == 'dev':
-from .dev import
+    from .prod import *
+elif env_name == 'stage':
+    from .stage import *
 else:
-from .local import *
+    from .local import *
